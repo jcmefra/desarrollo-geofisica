70e56841c336558dec6007ebcc26915b72981917
Camilo Mejía <camilomejia55@gmail.com>
2023-08-20 21:14:55 -0500

    minor adjustments

--- geoelectrica/Guapotá/proc2.py
+++ geoelectrica/Guapotá/proc2.py
@@ -1,20 +1,20 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pygimli as pg
+#Import dependencies
 from pygimli.physics import ert
-import pygimli.meshtools as mt
 
+#Load data
 data = ert.load('Guapota_Res.dat')
 
-
+#Show apparent resistivity data
 ax, cb = ert.show(data)
 
+#Compute geometric factor and error
 data["k"] = ert.geometricFactors(data)
 data["err"] = ert.estimateError(data, relativeError=0.02, absoluteUError=50e-6)
 
+#Initialize ERTManager and run inversion
 mgr = ert.ERTManager(data, verbose = True)
 
 inv = mgr.invert()
 
+#Show inversion and apparent resistivity (subplots)
 ax, cb = mgr.showResultAndFit()
-#meshPD = pg.Mesh(mgr.paraDomain) 
