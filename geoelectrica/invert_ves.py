99a3ea6f8b0ceb90527456b1d62549fdd936f809
Camilo Mejía <camilomejia55@gmail.com>
2023-09-06 22:59:25 -0500

    Seismic

--- geoelectrica/invert_ves.py
+++ geoelectrica/invert_ves.py
@@ -8,17 +8,15 @@ mn2 = [mn/2 for mn in (txt[:,1])]
 rhoa = (txt[:,6])
 
 ves = VESManager()
-# %%%
+
 ves.invert(data=rhoa, error=0.1, ab2=ab2, mn2=mn2,
            nLayers=4,
            # startModel=[3]*3+[100]*4,
            lam=1000, lambdaFactor=0.8
            )
 
-# %%%
-# show estimated&synthetic models and data with model response in 2 subplots
+# show estimated & synthetic models and data with model response in 2 subplots
 fig, ax = pg.plt.subplots(ncols=2, figsize=(8, 6))  # two-column figure
-#ves.showModel(synthModel, ax=ax[0], label="synth", plot="semilogy", zmax=20)
 ves.showModel(ves.model, ax=ax[0], label="model", zmax=200)
 ves.showData(rhoa, ax=ax[1], label="data", color="C0", marker="x")
 ves.showData(ves.inv.response, ax=ax[1], label="response", color="C1")
